300e3c998c1eb71b9aa169752ab2ef847d1385c3
oscardelatorre <oscardelatorre@localhost>
2021-12-13 23:37:56 +0100

    Update

--- src/no/systema/jservices/nortrailstats/controller/JsonResponseOutputterController_MAIN.java
+++ src/no/systema/jservices/nortrailstats/controller/JsonResponseOutputterController_MAIN.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.builder.ReflectionToStringBuilder;
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.ServletRequestDataBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,7 +34,7 @@ import no.systema.jservices.common.util.StringUtils;
  */
 @RestController
 public class JsonResponseOutputterController_MAIN {
-	private static final Logger logger = Logger.getLogger(JsonResponseOutputterController_MAIN.class.getName());
+	private static final Logger logger = LogManager.getLogger(JsonResponseOutputterController_MAIN.class.getName());
 	private DateTimeManager dateManager = new DateTimeManager();
 	private Integer DAFAULT_NUMBER_OF_DAYS_SQL = -10;
 	
